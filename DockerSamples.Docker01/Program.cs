139d27795c4db666dd5f09736d86a354f98c1ebc
korayozdemir <koray_ozdemir@msn.com>
2020-04-30 02:46:25 +0300

    Add project files.

---
+++ DockerSamples.Docker01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace DockerSamples.Docker01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                Console.WriteLine("Hello World!");
+                Thread.Sleep(1000);
+            }
+
+        }
+    }
+}
